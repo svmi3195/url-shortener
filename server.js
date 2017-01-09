7933cf4d37476efd5f685110dddceda3d0d1cbe8
svmi3195 <target1983@gmail.com>
2017-01-09 22:09:21 +0200

    started working on db

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://short:short@ds159328.mlab.com:59328/url-shortener-data');
+
+var Schema = mongoose.Schema;
+ 
+var linkSchema = new Schema({
+    original  : String,
+    shortened : String
+});
+
+var Link = mongoose.model('Link', linkSchema);
+
+var google = Link({
+    original  : 'https://www.google.ru/',
+    shortened : '12345'
+});
+
+google.save(function(error){
+    if(error) throw error;
+
+    console,log('input saved');
+})
 
 app.get('/*', function(req, res){
 
